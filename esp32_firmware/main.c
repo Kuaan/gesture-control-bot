cd2fe58335ce2c55b62312b9a054d1ac48bcde3b
Kuaan <89236757+Kuaan@users.noreply.github.com>
2025-06-10 13:18:51 +0800

    Update main.c

--- esp32_firmware/main.c
+++ esp32_firmware/main.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include "driver/gpio.h"
+#include "driver/uart.h"
+
+#define LED_PIN GPIO_NUM_2
+
+void app_main() {
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    uart_driver_install(UART_NUM_0, 1024, 0, 0, NULL, 0);
+
+    uint8_t data[128];
+    while (1) {
+        int len = uart_read_bytes(UART_NUM_0, data, sizeof(data), 100 / portTICK_PERIOD_MS);
+        if (len > 0) {
+            if (data[0] == 'f') {
+                gpio_set_level(LED_PIN, 1);  // turn LED on
+            } else if (data[0] == 's') {
+                gpio_set_level(LED_PIN, 0);  // turn LED off
+            }
+        }
+    }
+}
 
