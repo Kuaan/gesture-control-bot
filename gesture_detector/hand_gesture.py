6d63d0c503067adb721be1cc48fd170fc316b088
Kuaan <89236757+Kuaan@users.noreply.github.com>
2025-06-10 13:15:52 +0800

    Update hand_gesture.py

--- gesture_detector/hand_gesture.py
+++ gesture_detector/hand_gesture.py
@@ -1,1 +1,30 @@
+import cv2
+import mediapipe as mp
+
+mp_hands = mp.solutions.hands
+hands = mp_hands.Hands()
+mp_draw = mp.solutions.drawing_utils
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    # Flip the picture to achieve a mirror effect
+    frame = cv2.flip(frame, 1)
+    rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    result = hands.process(rgb)
+
+    if result.multi_hand_landmarks:
+        for hand_landmark in result.multi_hand_landmarks:
+            mp_draw.draw_landmarks(frame, hand_landmark, mp_hands.HAND_CONNECTIONS)
+
+    cv2.imshow("Hand Tracking", frame)
+    if cv2.waitKey(1) & 0xFF == 27:  # ESC quit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
 
