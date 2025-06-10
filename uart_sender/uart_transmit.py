c37fa61b624c4818964daa1d2c3e5657f7a84981
Kuaan <89236757+Kuaan@users.noreply.github.com>
2025-06-10 13:17:50 +0800

    Update uart_transmit.py

--- uart_sender/uart_transmit.py
+++ uart_sender/uart_transmit.py
@@ -1,1 +1,14 @@
+import serial
+import time
+
+ser = serial.Serial('COM3', 115200)  #### Setting the COM port depenced on your ESP32 
+
+def send_command(cmd: str):
+    ser.write(f"{cmd}\n".encode())
+    print(f"Sent: {cmd}")
+
+if __name__ == "__main__":
+    while True:
+        cmd = input("Enter command (f = forward, s = stop): ")
+        send_command(cmd)
 
